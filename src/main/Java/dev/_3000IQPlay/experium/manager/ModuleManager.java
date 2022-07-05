4d46d20286e86b3813ee13a9b9f8369c8ab520fa
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-07-05 17:35:47 +0200

    Anti-PistonFag.java

--- src/main/Java/dev/_3000IQPlay/experium/manager/ModuleManager.java
+++ src/main/Java/dev/_3000IQPlay/experium/manager/ModuleManager.java
@@ -153,8 +153,8 @@ public class ModuleManager
 		this.modules.add(new AntiSpam());
 		this.modules.add(new EnchantColor());
 		this.modules.add(new ChinaHat());
-		//this.modules.add(new AutoFrameDupe());
-		//this.moduleColorMap.put(this.getModuleByClass(AutoFrameDupe.class), new Color(63, 0, 158));
+		this.modules.add(new Blocker());
+		this.moduleColorMap.put(this.getModuleByClass(Blocker.class), new Color(63, 0, 158));
 		this.moduleColorMap.put(this.getModuleByClass(ChinaHat.class), new Color(0, 234, 78));
 		this.moduleColorMap.put(this.getModuleByClass(EnchantColor.class), new Color(66, 119, 9));
 		this.moduleColorMap.put(this.getModuleByClass(AntiSpam.class), new Color(192, 225, 0));
