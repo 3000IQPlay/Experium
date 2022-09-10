64b89ffef1bd6b66243837f87b8974d6938c677a
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-09-10 15:17:11 +0200

    trolla

--- src/main/Java/dev/_3000IQPlay/experium/features/gui/components/items/buttons/Slider.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/gui/components/items/buttons/Slider.java
@@ -32,6 +32,10 @@ public class Slider
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.dragSetting(mouseX, mouseY);
         RenderUtil.drawRect(this.x, this.y, this.x + (float)this.width + 7.4f, this.y + (float)this.height - 0.5f, !this.isHovering(mouseX, mouseY) ? 0x11555555 : -2007673515);
+		if (ClickGui.getInstance().sideSettings.getValue().booleanValue()) {
+            int sideColor = ColorUtil.toRGBA(ClickGui.getInstance().sideRed.getValue(), ClickGui.getInstance().sideGreen.getValue(), ClickGui.getInstance().sideBlue.getValue(), ClickGui.getInstance().sideAlpha.getValue());
+            RenderUtil.drawRect(this.x, this.y, this.x + 1.0f, this.y + (float)this.height + 1.0f, sideColor);
+        }
         if (ClickGui.getInstance().rainbowRolling.getValue().booleanValue()) {
             int color = ColorUtil.changeAlpha(HUD.getInstance().colorMap.get(MathUtil.clamp((int)this.y, 0, this.renderer.scaledHeight)), Experium.moduleManager.getModuleByClass(ClickGui.class).hoverAlpha.getValue());
             int color1 = ColorUtil.changeAlpha(HUD.getInstance().colorMap.get(MathUtil.clamp((int)this.y + this.height, 0, this.renderer.scaledHeight)), Experium.moduleManager.getModuleByClass(ClickGui.class).hoverAlpha.getValue());
