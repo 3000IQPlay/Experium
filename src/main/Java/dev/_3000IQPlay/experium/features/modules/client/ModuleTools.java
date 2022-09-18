3f45ba7cb4494ce86f74a57401382192fbf96320
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-09-18 08:44:43 +0200

    :scream:

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/client/ModuleTools.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/client/ModuleTools.java
@@ -2,12 +2,14 @@ package dev._3000IQPlay.experium.features.modules.client;
 
 import dev._3000IQPlay.experium.features.modules.Module;
 import dev._3000IQPlay.experium.features.setting.Setting;
+import dev._3000IQPlay.experium.util.TextUtil;
 
 public class ModuleTools
         extends Module {
     private static ModuleTools INSTANCE;
     public Setting<Notifier> notifier = register(new Setting<Notifier>("ModuleNotifier", Notifier.EXPERIUM));
     public Setting<PopNotifier> popNotifier = register(new Setting<PopNotifier>("PopNotifier", PopNotifier.NONE));
+	public Setting<TextUtil.Color> abyssColor = this.register(new Setting<TextUtil.Color>("AbyssTextColor", TextUtil.Color.AQUA, color -> this.notifier.getValue() == Notifier.ABYSS));
 
     public ModuleTools() {
         super("ModuleTools", "Change settings", Category.CLIENT, true, false, false);
@@ -34,7 +36,9 @@ public class ModuleTools
         KONAS,
         RUSHERHACK,
         LEGACY,
-        EUROPA;
+        EUROPA,
+		ABYSS,
+        LUIGIHACK;
     }
 
     public static enum PopNotifier {
