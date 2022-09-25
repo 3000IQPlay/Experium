4cb5bae415060574cdd2681a5e4ec0af2d9e34b7
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-09-25 16:56:42 +0200

    sry but its too good

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/NoSlowDown.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/NoSlowDown.java
@@ -2,56 +2,41 @@ package dev._3000IQPlay.experium.features.modules.movement;
 
 import dev._3000IQPlay.experium.event.events.KeyEvent;
 import dev._3000IQPlay.experium.event.events.PacketEvent;
+import dev._3000IQPlay.experium.event.events.PlayerUpdateMoveEvent;
 import dev._3000IQPlay.experium.features.modules.Module;
 import dev._3000IQPlay.experium.features.setting.Setting;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBow;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemPotion;
+import net.minecraft.init.Blocks;
 import net.minecraft.network.play.client.CPacketEntityAction;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketPlayerDigging;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MovementInput;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.InputUpdateEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
 
 public class NoSlowDown
         extends Module {
-    private static final KeyBinding[] keys = new KeyBinding[]{NoSlowDown.mc.gameSettings.keyBindForward, NoSlowDown.mc.gameSettings.keyBindBack, NoSlowDown.mc.gameSettings.keyBindLeft, NoSlowDown.mc.gameSettings.keyBindRight, NoSlowDown.mc.gameSettings.keyBindJump, NoSlowDown.mc.gameSettings.keyBindSprint};
-    private static NoSlowDown INSTANCE = new NoSlowDown();
-    public Setting<Boolean> guiMove = this.register(new Setting<Boolean>("GuiMove", true));
-    public Setting<Boolean> noSlow = this.register(new Setting<Boolean>("NoSlow", true));
-    public Setting<Boolean> soulSand = this.register(new Setting<Boolean>("SoulSand", false));
-    public Setting<Boolean> strict = this.register(new Setting<Boolean>("Strict", false));
-    public Setting<Boolean> sneakPacket = this.register(new Setting<Boolean>("SneakPacket", false));
-    public Setting<Boolean> endPortal = this.register(new Setting<Boolean>("EndPortal", false));
-    private boolean sneaking = false;
-
-    public NoSlowDown() {
+	private static final KeyBinding[] keys = new KeyBinding[]{NoSlowDown.mc.gameSettings.keyBindForward, NoSlowDown.mc.gameSettings.keyBindBack, NoSlowDown.mc.gameSettings.keyBindLeft, NoSlowDown.mc.gameSettings.keyBindRight, NoSlowDown.mc.gameSettings.keyBindJump, NoSlowDown.mc.gameSettings.keyBindSprint};
+	public Setting<ItemMode> itemMode = this.register(new Setting<ItemMode>("ItemMode", ItemMode.Normal));
+	public Setting<Boolean> items = this.register(new Setting<Boolean>("Items", true));
+	public Setting<Boolean> guiMove = this.register(new Setting<Boolean>("GuiMove", true));
+    public Setting<Boolean> sneak = this.register(new Setting<Boolean>("Sneak", false));
+	boolean sneakingFlag;
+	
+	public NoSlowDown() {
         super("NoSlowDown", "Prevents you from getting slowed down.", Module.Category.MOVEMENT, true, false, false);
-        this.setInstance();
     }
-
-    public static NoSlowDown getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new NoSlowDown();
-        }
-        return INSTANCE;
-    }
-
-    private void setInstance() {
-        INSTANCE = this;
-    }
-
-    @Override
-    public void onUpdate() {
-        if (this.guiMove.getValue().booleanValue()) {
+	
+    @SubscribeEvent
+    public void onUpdate(PlayerUpdateMoveEvent event) {
+		if (this.guiMove.getValue().booleanValue()) {
             if (NoSlowDown.mc.currentScreen instanceof GuiOptions || NoSlowDown.mc.currentScreen instanceof GuiVideoSettings || NoSlowDown.mc.currentScreen instanceof GuiScreenOptionsSounds || NoSlowDown.mc.currentScreen instanceof GuiContainer || NoSlowDown.mc.currentScreen instanceof GuiIngameMenu) {
                 for (KeyBinding bind : keys) {
                     KeyBinding.setKeyBindState(bind.getKeyCode(), Keyboard.isKeyDown(bind.getKeyCode()));
@@ -63,31 +48,27 @@ public class NoSlowDown
                 }
             }
         }
-        Item item = NoSlowDown.mc.player.getActiveItemStack().getItem();
-        if (this.sneaking && !NoSlowDown.mc.player.isHandActive() && this.sneakPacket.getValue().booleanValue()) {
-            NoSlowDown.mc.player.connection.sendPacket(new CPacketEntityAction(NoSlowDown.mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
-            this.sneaking = false;
-        }
-    }
-
-    @SubscribeEvent
-    public void onUseItem(PlayerInteractEvent.RightClickItem event) {
-        Item item = NoSlowDown.mc.player.getHeldItem(event.getHand()).getItem();
-        if ((item instanceof ItemFood || item instanceof ItemBow || item instanceof ItemPotion && this.sneakPacket.getValue().booleanValue()) && !this.sneaking) {
-            NoSlowDown.mc.player.connection.sendPacket(new CPacketEntityAction(NoSlowDown.mc.player, CPacketEntityAction.Action.START_SNEAKING));
-            this.sneaking = true;
+        if (this.itemMode.getValue() == ItemMode._2B2TSneak && !mc.player.isSneaking() && !mc.player.isRiding()) {
+            if (mc.player.isHandActive()) {
+                mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SNEAKING));
+                sneakingFlag = true;
+            }
+            else if (sneakingFlag) {
+                mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
+                sneakingFlag = false;
+            }
         }
     }
 
     @SubscribeEvent
     public void onInput(InputUpdateEvent event) {
-        if (this.noSlow.getValue().booleanValue() && NoSlowDown.mc.player.isHandActive() && !NoSlowDown.mc.player.isRiding()) {
-            event.getMovementInput().moveStrafe *= 5.0f;
-            event.getMovementInput().moveForward *= 5.0f;
+        if ((this.items.getValue().booleanValue() && mc.player.isHandActive() && !mc.player.isRiding()) || (this.sneak.getValue().booleanValue() && mc.player.isSneaking())) {
+            event.getMovementInput().moveForward /= 0.2f;
+            event.getMovementInput().moveStrafe /= 0.2f;
         }
     }
-
-    @SubscribeEvent
+	
+	@SubscribeEvent
     public void onKeyEvent(KeyEvent event) {
         if (this.guiMove.getValue().booleanValue() && event.getStage() == 0 && !(NoSlowDown.mc.currentScreen instanceof GuiChat)) {
             event.info = event.pressed;
@@ -95,10 +76,26 @@ public class NoSlowDown
     }
 
     @SubscribeEvent
-    public void onPacket(PacketEvent.Send event) {
-        if (event.getPacket() instanceof CPacketPlayer && this.strict.getValue().booleanValue() && this.noSlow.getValue().booleanValue() && NoSlowDown.mc.player.isHandActive() && !NoSlowDown.mc.player.isRiding()) {
-            NoSlowDown.mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, new BlockPos(Math.floor(NoSlowDown.mc.player.posX), Math.floor(NoSlowDown.mc.player.posY), Math.floor(NoSlowDown.mc.player.posZ)), EnumFacing.DOWN));
+    public void onPacketSend(PacketEvent.Send event) {
+        if (event.getPacket() instanceof CPacketPlayer && this.items.getValue().booleanValue() && mc.player.isHandActive() && !mc.player.isRiding()) {
+            switch (this.itemMode.getValue()) {
+                case NCPStrict: {
+                    mc.player.connection.sendPacket(new CPacketPlayerDigging(CPacketPlayerDigging.Action.ABORT_DESTROY_BLOCK, new BlockPos(Math.floor(mc.player.posX), Math.floor(mc.player.posY), Math.floor(mc.player.posZ)), EnumFacing.DOWN));
+                    break;
+                }
+                case _2B2TBypass: {
+                    mc.player.connection.sendPacket(new CPacketHeldItemChange(mc.player.inventory.currentItem));
+                    break;
+                }
+            }
         }
     }
-}
 
+
+    public enum ItemMode {
+        Normal,
+        NCPStrict,
+        _2B2TSneak,
+        _2B2TBypass;
+    }
+}
