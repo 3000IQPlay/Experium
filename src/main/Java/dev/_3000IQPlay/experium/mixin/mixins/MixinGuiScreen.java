35ea087e8412bc489185fbc3c270f35347a74566
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-09-29 19:49:42 +0200

    bruv

--- src/main/Java/dev/_3000IQPlay/experium/mixin/mixins/MixinGuiScreen.java
+++ src/main/Java/dev/_3000IQPlay/experium/mixin/mixins/MixinGuiScreen.java
@@ -1,6 +1,7 @@
 package dev._3000IQPlay.experium.mixin.mixins;
 
 import dev._3000IQPlay.experium.features.modules.misc.ToolTips;
+import dev._3000IQPlay.experium.features.modules.render.NoRender;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemShulkerBox;
@@ -17,6 +18,13 @@ public class MixinGuiScreen
     public void renderToolTipHook(ItemStack stack, int x, int y, CallbackInfo info) {
         if (ToolTips.getInstance().isOn() && ToolTips.getInstance().shulkers.getValue().booleanValue() && stack.getItem() instanceof ItemShulkerBox) {
             ToolTips.getInstance().renderShulkerToolTip(stack, x, y, null);
+            info.cancel();
+        }
+    }
+	
+	@Inject(method = "drawDefaultBackground", at = @At("HEAD"), cancellable = true)
+    public void drawDefaultBackground(CallbackInfo info) {
+        if (NoRender.getInstance().isOn() && NoRender.getInstance().containerBackground.getValue().booleanValue()){
             info.cancel();
         }
     }
