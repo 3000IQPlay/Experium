e30f29eb4f3d0c741f9b72e4024e4ff938643562
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-10-02 19:24:30 +0200

    Collision? yez

---
+++ src/main/Java/dev/_3000IQPlay/experium/event/EntityCollisionEvent.java
@@ -0,0 +1,9 @@
+package dev._3000IQPlay.experium.event.events;
+
+import dev._3000IQPlay.experium.event.EventStage;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+
+@Cancelable
+public class EntityCollisionEvent
+        extends EventStage {
+}
