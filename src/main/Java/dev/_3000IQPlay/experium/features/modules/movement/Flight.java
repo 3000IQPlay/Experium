2c2c5a3d5a31fe6a1f82fd817d1bb0ad791af393
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-10-26 09:14:30 +0200

    Update Flight.java

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/Flight.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/Flight.java
@@ -1,20 +1,35 @@
 package dev._3000IQPlay.experium.features.modules.movement;
 
 import dev._3000IQPlay.experium.Experium;
+import dev._3000IQPlay.experium.event.events.MoveEvent;
 import dev._3000IQPlay.experium.features.modules.Module;
 import dev._3000IQPlay.experium.features.setting.Setting;
 import dev._3000IQPlay.experium.util.EntityUtil;
+import dev._3000IQPlay.experium.util.MathUtil;
 import dev._3000IQPlay.experium.util.Timer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerCapabilities;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.MobEffects;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketConfirmTeleport;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
+import net.minecraft.network.play.server.SPacketPlayerPosLook;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class Flight
         extends Module {
     private static Flight INSTANCE = new Flight();
     public Setting<FlyMode> flyMode = this.register(new Setting<FlyMode>("FlyType", FlyMode.Motion));
-	public Setting<Float> verticalSpeed = this.register(new Setting<Float>("VerticalSpeed", 0.5f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
+	public Setting<Float> jumpSpeed = this.register(new Setting<Float>("yPortSpeed", 3.25f, 2.0f, 10.0f, v -> this.flyMode.getValue() == FlyMode.yPort));
 	public Setting<Float> hrizontalSpeed = this.register(new Setting<Float>("HorizontalSpeed", 0.5f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
 	public Setting<Float> timerSpeed = this.register(new Setting<Float>("Timer", 1.0f, 0.1f, 5.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
-	public Setting<Float> jumpMotion = this.register(new Setting<Float>("JumpMotionY", 0.42f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.AirJump));
+	public Setting<Float> jumpMotion = this.register(new Setting<Float>("JumpMotionY", 0.42f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.ManualAirJump));
 	public Timer timer = new Timer();
 
     public Flight() {
@@ -30,8 +45,21 @@ public class Flight
 	        Experium.timerManager.setTimer(this.timerSpeed.getValue().floatValue());
 	        EntityUtil.moveEntityStrafe(this.hrizontalSpeed.getValue().floatValue(), (Entity)Flight.mc.player);
 		}
-		if (this.flyMode.getValue() == FlyMode.AirJump) {
+		if (this.flyMode.getValue() == FlyMode.ManualAirJump) {
 		    Flight.mc.player.onGround = true;
+		}
+		if (this.flyMode.getValue() == FlyMode.Vanilla) {
+    		Flight.mc.player.capabilities.isFlying = true;
+		}
+		if (this.flyMode.getValue() == FlyMode.yPort) {
+			if (Flight.mc.player.onGround) {
+				Flight.mc.player.jump();
+			}
+			EntityUtil.moveEntityStrafe(this.jumpSpeed.getValue().floatValue() / 10, Flight.mc.player);
+			if (Flight.mc.player.fallDistance > 1) {
+				Flight.mc.player.motionY = 0.5D;
+				Flight.mc.player.fallDistance = 0F;
+			}
 		}
     }
 	
@@ -54,6 +82,8 @@ public class Flight
 	
 	public static enum FlyMode {
 		Motion,
-		AirJump;
+		Vanilla,
+		yPort,
+		ManualAirJump;
 	}
 }
