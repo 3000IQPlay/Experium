b44b47a1878ee1a8276fd4f3be6c1fd951dddfae
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-10-30 10:54:06 +0100

    Update Flight.java

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/Flight.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/movement/Flight.java
@@ -1,13 +1,17 @@
 package dev._3000IQPlay.experium.features.modules.movement;
 
 import dev._3000IQPlay.experium.Experium;
+import dev._3000IQPlay.experium.event.events.MoveEvent;
 import dev._3000IQPlay.experium.event.events.PacketEvent;
 import dev._3000IQPlay.experium.features.modules.Module;
 import dev._3000IQPlay.experium.features.setting.Setting;
 import dev._3000IQPlay.experium.util.EntityUtil;
 import dev._3000IQPlay.experium.util.Timer;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketKeepAlive;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.potion.Potion;
@@ -17,15 +21,18 @@ public class Flight
         extends Module {
     private static Flight INSTANCE = new Flight();
     public Setting<FlyMode> flyMode = this.register(new Setting<FlyMode>("FlyType", FlyMode.Motion));
-	public Setting<Float> vanillaSpeed = this.register(new Setting<Float>("VanillaSpeed", 5.0f, 1.0f, 30.0f, v -> this.flyMode.getValue() == FlyMode.Vanilla));
+	public Setting<Boolean> reDamage = this.register(new Setting<Boolean>("ReDamage", true, v -> this.flyMode.getValue() == FlyMode.VerusBoost));
+	public Setting<Float> vbSpeed = this.register(new Setting<Float>("VerusBoostSpeed", 15.0f, 2.0f, 30.0f, v -> this.flyMode.getValue() == FlyMode.VerusBoost));
+	public Setting<Float> vanillaSpeed = this.register(new Setting<Float>("VanillaSpeed", 5.0f, 2.0f, 30.0f, v -> this.flyMode.getValue() == FlyMode.Vanilla));
 	public Setting<Float> verticalSpeed = this.register(new Setting<Float>("VerticalSpeed", 10.0f, 1.0f, 10.0f, v -> this.flyMode.getValue() == FlyMode.Vanilla));
 	public Setting<Boolean> keepAlive = this.register(new Setting<Boolean>("KeepAlive", false, v -> this.flyMode.getValue() == FlyMode.Vanilla));
 	public Setting<Boolean> noClip = this.register(new Setting<Boolean>("NoClip", false, v -> this.flyMode.getValue() == FlyMode.Vanilla));
 	public Setting<Boolean> spoof = this.register(new Setting<Boolean>("SpoofGround", false, v -> this.flyMode.getValue() == FlyMode.Vanilla));
 	public Setting<Float> jumpSpeed = this.register(new Setting<Float>("AutoAirJumpSpeed", 3.25f, 2.0f, 10.0f, v -> this.flyMode.getValue() == FlyMode.AutoAirJump));
-	public Setting<Float> mHorizontalSpeed = this.register(new Setting<Float>("HorizontalSpeed", 0.5f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
+	public Setting<Float> mHorizontalSpeed = this.register(new Setting<Float>("HorizontalSpeed", 5.0f, 2.0f, 3.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
 	public Setting<Float> mTimerSpeed = this.register(new Setting<Float>("Timer", 1.0f, 0.1f, 5.0f, v -> this.flyMode.getValue() == FlyMode.Motion));
 	public Setting<Float> jumpMotion = this.register(new Setting<Float>("JumpMotionY", 0.42f, 0.1f, 3.0f, v -> this.flyMode.getValue() == FlyMode.ManualAirJump));
+	private int ticks = 0;
 
     public Flight() {
         super("Flight", "Makes you fly.", Module.Category.MOVEMENT, true, false, false);
@@ -44,6 +51,11 @@ public class Flight
     }
 	
 	@Override
+	public void onEnable() {
+		this.ticks = 1;
+    }
+	
+	@Override
 	public void onUpdate() {
 		if (this.flyMode.getValue() == FlyMode.Motion) {
 		    Flight.mc.player.motionY = 0.0;
@@ -55,6 +67,7 @@ public class Flight
 		    Flight.mc.player.onGround = true;
 		}
 		if (this.flyMode.getValue() == FlyMode.AutoAirJump) {
+			Experium.timerManager.reset();
 			if (Flight.mc.player.onGround) {
 				Flight.mc.player.jump();
 			}
@@ -65,8 +78,9 @@ public class Flight
 			}
 		}
 		if (this.flyMode.getValue() == FlyMode.Vanilla) {
+			Experium.timerManager.reset();
 			if (this.keepAlive.getValue().booleanValue()) {
-                Flight.mc.player.connection.sendPacket(new CPacketKeepAlive());
+                Flight.mc.player.connection.sendPacket((Packet)new CPacketKeepAlive());
             }
             if (this.noClip.getValue().booleanValue()) {
                 Flight.mc.player.noClip = true;
@@ -82,11 +96,49 @@ public class Flight
             EntityUtil.moveEntityStrafe(this.vanillaSpeed.getValue().floatValue() / 10, (Entity)Flight.mc.player);
 		}
 		if (this.flyMode.getValue() == FlyMode.Jetpack) {
+			Experium.timerManager.reset();
 			if (Flight.mc.gameSettings.keyBindJump.isKeyDown()) {
                 Flight.mc.player.motionY += 0.15;
                 Flight.mc.player.motionX *= 1.1;
                 Flight.mc.player.motionZ *= 1.1;
 			}
+        }
+		if (this.flyMode.getValue() == FlyMode.VerusBoost) {
+			if (this.ticks == 1) {
+            Flight.mc.player.connection.sendPacket((Packet)new CPacketEntityAction(Flight.mc.player, CPacketEntityAction.Action.START_SPRINTING));
+            Flight.mc.player.connection.sendPacket((Packet)new CPacketPlayer.Position(Flight.mc.player.posX, Flight.mc.player.posY, Flight.mc.player.posZ, true));
+            Flight.mc.player.connection.sendPacket((Packet)new CPacketPlayer.Position(Flight.mc.player.posX, Flight.mc.player.posY + 3.42, Flight.mc.player.posZ, false));
+            Flight.mc.player.connection.sendPacket((Packet)new CPacketPlayer.Position(Flight.mc.player.posX, Flight.mc.player.posY, Flight.mc.player.posZ, false));
+            Flight.mc.player.connection.sendPacket((Packet)new CPacketPlayer.Position(Flight.mc.player.posX, Flight.mc.player.posY, Flight.mc.player.posZ, true));
+            Experium.timerManager.setTimer(0.15f);
+            Flight.mc.player.jump();
+            Flight.mc.player.onGround = true;
+            } else if (this.ticks == 2) {
+                Experium.timerManager.reset();
+            }
+            if (Flight.mc.player.onGround) {
+                Flight.mc.player.jump();
+            }
+            if (Flight.mc.player.fallDistance > 1) {
+                Flight.mc.player.motionY = -((Flight.mc.player.posY) - Math.floor(Flight.mc.player.posY));
+            }
+            if (Flight.mc.player.motionY == 0.0) {
+                Flight.mc.player.jump();
+                Flight.mc.player.onGround = true;
+                Flight.mc.player.fallDistance = 0f;
+            }
+            if (this.ticks < 25) {
+                EntityUtil.moveEntityStrafe(this.vbSpeed.getValue().floatValue() / 10, (Entity)Flight.mc.player);
+            } else {
+                if (this.ticks == 25){
+                    EntityUtil.moveEntityStrafe(0.48f, (Entity)Flight.mc.player);
+                }
+                if (this.reDamage.getValue().booleanValue()) {
+                    this.ticks = 1;
+                }
+                EntityUtil.moveEntityStrafe(Math.sqrt(mc.player.motionX * mc.player.motionX + mc.player.motionZ * mc.player.motionZ), (Entity)Flight.mc.player);
+            }
+			this.ticks++;
         }
     }
 	
@@ -111,6 +163,8 @@ public class Flight
 		Motion,
 		Vanilla,
 		Jetpack,
+		VerusBoost,
+		ZoneCraft,
 		AutoAirJump,
 		ManualAirJump;
 	}
