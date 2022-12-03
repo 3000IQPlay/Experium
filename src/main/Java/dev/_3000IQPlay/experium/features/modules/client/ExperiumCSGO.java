c25c3998e78f37bcce08a998394e55a50f75f7e2
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-12-03 09:59:43 +0100

    Updated ExperiumCSGO.java (CP Applied)

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/client/ExperiumCSGO.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/client/ExperiumCSGO.java
@@ -20,35 +20,15 @@ import java.util.Objects;
 
 public class ExperiumCSGO
         extends Module {
-	public Setting<Pages> page = this.register(new Setting<Pages>("Page", Pages.Watermark));
-	public Setting<Colorsp> colorp = this.register(new Setting<Colorsp>("ColorPage", Colorsp.First, v -> this.page.getValue() == Pages.BarColor));
-	public Setting<Integer> posY = this.register(new Setting<Integer>("PosY", 2, 0, 512, v -> this.page.getValue() == Pages.Watermark));
-	public Setting<Double> barPosY = this.register(new Setting<Double>("BarPosY", 0.0, -2.0, 15.0, v -> this.page.getValue() == Pages.Watermark));
-	public Setting<Double> textPosY = this.register(new Setting<Double>("TextPosY", 0.0, -6.0, 3.0, v -> this.page.getValue() == Pages.Watermark));
-	public Setting<Integer> bgRed = this.register(new Setting<Integer>("BackgroundRed", 20, 0, 255, v -> this.page.getValue() == Pages.Background));
-	public Setting<Integer> bgGreen = this.register(new Setting<Integer>("BackgroundGreen", 20, 0, 255, v -> this.page.getValue() == Pages.Background));
-	public Setting<Integer> bgBlue = this.register(new Setting<Integer>("BackgroundBlue", 20, 0, 255, v -> this.page.getValue() == Pages.Background));
-	public Setting<Integer> bgAlpha = this.register(new Setting<Integer>("BackgroundAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.Background));
-	public Setting<Integer> bRed = this.register(new Setting<Integer>("BorderRed", 0, 0, 255, v -> this.page.getValue() == Pages.Border));
-	public Setting<Integer> bGreen = this.register(new Setting<Integer>("BorderGreen", 0, 0, 255, v -> this.page.getValue() == Pages.Border));
-	public Setting<Integer> bBlue = this.register(new Setting<Integer>("BorderBlue", 0, 0, 255, v -> this.page.getValue() == Pages.Border));
-	public Setting<Integer> bAlpha = this.register(new Setting<Integer>("BorderAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.Border));
-	public Setting<Integer> firstRed = this.register(new Setting<Integer>("FirstRed", 135, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.First));
-	public Setting<Integer> firstGreen = this.register(new Setting<Integer>("FirstGreen", 135, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.First));
-	public Setting<Integer> firtBlue = this.register(new Setting<Integer>("FirstBlue", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.First));
-	public Setting<Integer> firstAlpha = this.register(new Setting<Integer>("FirstAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.First));
-	public Setting<Integer> secondRed = this.register(new Setting<Integer>("SecondRed", 0, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Second));
-	public Setting<Integer> secondGreen = this.register(new Setting<Integer>("SecondGreen", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Second));
-	public Setting<Integer> secondBlue = this.register(new Setting<Integer>("SecondBlue", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Second));
-	public Setting<Integer> secondAlpha = this.register(new Setting<Integer>("SecondAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Second));
-	public Setting<Integer> thirdRed = this.register(new Setting<Integer>("ThirdRed", 135, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Third));
-	public Setting<Integer> thirdGreen = this.register(new Setting<Integer>("ThirdGreen", 135, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Third));
-	public Setting<Integer> thirdBlue = this.register(new Setting<Integer>("ThirdBlue", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Third));
-	public Setting<Integer> thirdAlpha = this.register(new Setting<Integer>("ThirdAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Third));
-	public Setting<Integer> fourthRed = this.register(new Setting<Integer>("FourthRed", 0, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Fourth));
-	public Setting<Integer> fourthGreen = this.register(new Setting<Integer>("FourthGreen", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Fourth));
-	public Setting<Integer> fourthBlue = this.register(new Setting<Integer>("FourthBlue", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Fourth));
-	public Setting<Integer> fourthAlpha = this.register(new Setting<Integer>("FourthAlpha", 255, 0, 255, v -> this.page.getValue() == Pages.BarColor && this.colorp.getValue() == Colorsp.Fourth));
+	public Setting<Integer> posY = this.register(new Setting<Integer>("PosY", 2, 0, 512));
+	public Setting<Double> barPosY = this.register(new Setting<Double>("BarPosY", 0.0, -2.0, 15.0));
+	public Setting<Double> textPosY = this.register(new Setting<Double>("TextPosY", 0.0, -6.0, 3.0));
+	public Setting<Color> bgC = register(new Setting<Color>("BackgroundColor", new Color(20, 20, 20, 255)));
+	public Setting<Color> bC = register(new Setting<Color>("BorderColor", new Color(0, 0, 20, 255)));
+	public Setting<Color> fC = register(new Setting<Color>("FirstLineColor", new Color(135, 135, 255, 255)));
+	public Setting<Color> sC = register(new Setting<Color>("SecondLineColor", new Color(0, 255, 255, 255)));
+	public Setting<Color> tC = register(new Setting<Color>("ThirdLineColor", new Color(135, 135, 255, 255)));
+	public Setting<Color> foC = register(new Setting<Color>("FourthLineColor", new Color(0, 255, 255, 255)));
 	
     public ExperiumCSGO() {
         super("ExperiumCSGO", "Hot csgo watermark", Module.Category.CLIENT, true, false, false);
@@ -72,13 +52,13 @@ public class ExperiumCSGO
             int posY = this.posY.getValue();
 			double barPosY = this.barPosY.getValue();
 			double textPosY = this.textPosY.getValue();
-            RenderUtil.drawRectangleCorrectly(posX - 4, posY - 4, (int)(width + 10.0f), height + 6, ColorUtil.toRGBA(this.bRed.getValue(), this.bGreen.getValue(), this.bBlue.getValue(), this.bAlpha.getValue()));
-            RenderUtil.drawRectangleCorrectly(posX - 4, posY - 4, (int)(width + 11.0f), height + 7, ColorUtil.toRGBA(this.bRed.getValue(), this.bGreen.getValue(), this.bBlue.getValue(), this.bAlpha.getValue()));
-            ExperiumCSGO.drawRect(posX, posY, (float)posX + width + 2.0f, posY + height, new Color(this.bgRed.getValue(), this.bgGreen.getValue(), this.bgBlue.getValue(), this.bgAlpha.getValue()).getRGB());
-            ExperiumCSGO.drawRect((double)posX + 2.5, (double)posY + 2.5, (double)((float)posX + width) - 0.5, (double)posY + 4.5, new Color(this.bgRed.getValue(), this.bgGreen.getValue(), this.bgBlue.getValue(), this.bgAlpha.getValue()).getRGB());
-            ExperiumCSGO.drawGradientSideways(4.0, (posY + barPosY) + 3, 4.0f + width / 3.0f, (posY + barPosY) + 4, new Color(this.firstRed.getValue(), this.firstGreen.getValue(), this.firtBlue.getValue(), this.firstAlpha.getValue()).getRGB(), new Color(this.secondRed.getValue(), this.secondGreen.getValue(), this.secondBlue.getValue(), this.secondAlpha.getValue()).getRGB());
-            ExperiumCSGO.drawGradientSideways(4.0f + width / 3.0f, (posY + barPosY) + 3, 4.0f + width / 3.0f * 2.0f, (posY + barPosY) + 4, new Color(this.secondRed.getValue(), this.secondGreen.getValue(), this.secondBlue.getValue(), this.secondAlpha.getValue()).getRGB(), new Color(this.thirdRed.getValue(), this.thirdGreen.getValue(), this.thirdBlue.getValue(), this.thirdAlpha.getValue()).getRGB());
-            ExperiumCSGO.drawGradientSideways(4.0f + width / 3.0f * 2.0f, (posY + barPosY) + 3, width / 3.0f * 3.0f + 1.0f, (posY + barPosY) + 4, new Color(this.thirdRed.getValue(), this.thirdGreen.getValue(), this.thirdBlue.getValue(), this.thirdAlpha.getValue()).getRGB(), new Color(this.fourthRed.getValue(), this.fourthGreen.getValue(), this.fourthBlue.getValue(), this.fourthAlpha.getValue()).getRGB());
+            RenderUtil.drawRectangleCorrectly(posX - 4, posY - 4, (int)(width + 10.0f), height + 6, ColorUtil.toRGBA(this.bC.getValue().getRed(), this.bC.getValue().getGreen(), this.bC.getValue().getBlue(), this.bC.getValue().getAlpha()));
+            RenderUtil.drawRectangleCorrectly(posX - 4, posY - 4, (int)(width + 11.0f), height + 7, ColorUtil.toRGBA(this.bC.getValue().getRed(), this.bC.getValue().getGreen(), this.bC.getValue().getBlue(), this.bC.getValue().getAlpha()));
+            ExperiumCSGO.drawRect(posX, posY, (float)posX + width + 2.0f, posY + height, new Color(this.bgC.getValue().getRed(), this.bgC.getValue().getGreen(), this.bgC.getValue().getBlue(), this.bgC.getValue().getAlpha()).getRGB());
+            ExperiumCSGO.drawRect((double)posX + 2.5, (double)posY + 2.5, (double)((float)posX + width) - 0.5, (double)posY + 4.5, new Color(this.bgC.getValue().getRed(), this.bgC.getValue().getGreen(), this.bgC.getValue().getBlue(), this.bgC.getValue().getAlpha()).getRGB());
+            ExperiumCSGO.drawGradientSideways(4.0, (posY + barPosY) + 3, 4.0f + width / 3.0f, (posY + barPosY) + 4, new Color(this.fC.getValue().getRed(), this.fC.getValue().getGreen(), this.fC.getValue().getBlue(), this.fC.getValue().getAlpha()).getRGB(), new Color(this.sC.getValue().getRed(), this.sC.getValue().getGreen(), this.sC.getValue().getBlue(), this.sC.getValue().getAlpha()).getRGB());
+            ExperiumCSGO.drawGradientSideways(4.0f + width / 3.0f, (posY + barPosY) + 3, 4.0f + width / 3.0f * 2.0f, (posY + barPosY) + 4, new Color(this.sC.getValue().getRed(), this.sC.getValue().getGreen(), this.sC.getValue().getBlue(), this.sC.getValue().getAlpha()).getRGB(), new Color(this.tC.getValue().getRed(), this.tC.getValue().getGreen(), this.tC.getValue().getBlue(), this.tC.getValue().getAlpha()).getRGB());
+            ExperiumCSGO.drawGradientSideways(4.0f + width / 3.0f * 2.0f, (posY + barPosY) + 3, width / 3.0f * 3.0f + 1.0f, (posY + barPosY) + 4, new Color(this.tC.getValue().getRed(), this.tC.getValue().getGreen(), this.tC.getValue().getBlue(), this.tC.getValue().getAlpha()).getRGB(), new Color(this.foC.getValue().getRed(), this.foC.getValue().getGreen(), this.foC.getValue().getBlue(), this.foC.getValue().getAlpha()).getRGB());
             Minecraft.getMinecraft().fontRenderer.drawStringWithShadow(text, (float)(4 + posX), (float)(8 + (posY + textPosY)), -1);
         }
     }
@@ -151,19 +131,5 @@ public class ExperiumCSGO
             // empty catch block
         }
         return ping;
-    }
-	
-	public static enum Pages {
-		Watermark,
-		BarColor,
-        Background,
-        Border;
-    }
-	
-	public static enum Colorsp {
-		First,
-		Second,
-        Third,
-        Fourth;
     }
 }
