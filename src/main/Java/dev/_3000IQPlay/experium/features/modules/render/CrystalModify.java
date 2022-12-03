92419c40a9d13d8121f511b6c0b20f9d1ef89f18
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-12-03 21:27:52 +0100

    Update CrystalModify.java

--- src/main/Java/dev/_3000IQPlay/experium/features/modules/render/CrystalModify.java
+++ src/main/Java/dev/_3000IQPlay/experium/features/modules/render/CrystalModify.java
@@ -3,6 +3,8 @@ package dev._3000IQPlay.experium.features.modules.render;
 import dev._3000IQPlay.experium.features.modules.Module;
 import dev._3000IQPlay.experium.features.setting.Setting;
 
+import java.awt.Color;
+
 public class CrystalModify
         extends Module {
     public static CrystalModify INSTANCE;
@@ -13,27 +15,15 @@ public class CrystalModify
     public Setting<Float> crystalBounce = this.register(new Setting<Float>("Bounce", Float.valueOf(0.2f), Float.valueOf(0.1f), Float.valueOf(1.0f)));
     public Setting<BlendModes> blendModes = this.register(new Setting<BlendModes>("Blend", BlendModes.Default));
     public Setting<Boolean> enchanted = this.register(new Setting<Boolean>("Glint", false));
-    public Setting<Integer> enchantRed = this.register(new Setting<Object>("Glint Red", Integer.valueOf(135), Integer.valueOf(0), Integer.valueOf(255), v -> this.enchanted.getValue()));
-    public Setting<Integer> enchantGreen = this.register(new Setting<Object>("Glint Green", Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(255), v -> this.enchanted.getValue()));
-    public Setting<Integer> enchantBlue = this.register(new Setting<Object>("Glint Blue", Integer.valueOf(255), Integer.valueOf(0), Integer.valueOf(255), v -> this.enchanted.getValue()));
-    public Setting<Integer> enchantAlpha = this.register(new Setting<Object>("Glint Alpha", Integer.valueOf(255), Integer.valueOf(0), Integer.valueOf(255), v -> this.enchanted.getValue()));
+	public Setting<Color> glintC = this.register(new Setting<Color>("GlintColor", new Color(40, 192, 255, 255), v -> this.enchanted.getValue()));
     public Setting<Boolean> texture = this.register(new Setting<Boolean>("Texture", false));
     public Setting<Boolean> colorSync = this.register(new Setting<Boolean>("Sync", false));
-    public Setting<Integer> red = this.register(new Setting<Integer>("Red", 135, 0, 255));
-    public Setting<Integer> green = this.register(new Setting<Integer>("Green", 0, 0, 255));
-    public Setting<Integer> blue = this.register(new Setting<Integer>("Blue", 255, 0, 255));
-    public Setting<Integer> alpha = this.register(new Setting<Integer>("Alpha", 255, 0, 255));
+	public Setting<Color> colorC = this.register(new Setting<Color>("Color", new Color(40, 192, 255, 255)));
     public Setting<Boolean> outline = this.register(new Setting<Boolean>("Outline", false));
     public Setting<Float> lineWidth = this.register(new Setting<Float>("LineWidth", Float.valueOf(1.0f), Float.valueOf(0.1f), Float.valueOf(5.0f), v -> this.outline.getValue()));
-    public Setting<Integer> outlineRed = this.register(new Setting<Object>("Outline Red", Integer.valueOf(135), Integer.valueOf(0), Integer.valueOf(255), v -> this.outline.getValue()));
-    public Setting<Integer> outlineGreen = this.register(new Setting<Object>("Outline Green", Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(255), v -> this.outline.getValue()));
-    public Setting<Integer> outlineBlue = this.register(new Setting<Object>("Outline Blue", Integer.valueOf(255), Integer.valueOf(0), Integer.valueOf(255), v -> this.outline.getValue()));
-    public Setting<Integer> outlineAlpha = this.register(new Setting<Object>("Outline Alpha", Integer.valueOf(90), Integer.valueOf(0), Integer.valueOf(255), v -> this.outline.getValue()));
+	public Setting<Color> outlineC = this.register(new Setting<Color>("OutlineColor", new Color(40, 192, 255, 255), v -> this.outline.getValue()));
     public Setting<Boolean> hiddenSync = this.register(new Setting<Boolean>("Hidden Sync", false));
-    public Setting<Integer> hiddenRed = this.register(new Setting<Object>("Hidden Red", Integer.valueOf(135), Integer.valueOf(0), Integer.valueOf(255), v -> this.hiddenSync.getValue() == false));
-    public Setting<Integer> hiddenGreen = this.register(new Setting<Object>("Hidden Green", Integer.valueOf(0), Integer.valueOf(0), Integer.valueOf(255), v -> this.hiddenSync.getValue() == false));
-    public Setting<Integer> hiddenBlue = this.register(new Setting<Object>("Hidden Blue", Integer.valueOf(255), Integer.valueOf(0), Integer.valueOf(255), v -> this.hiddenSync.getValue() == false));
-    public Setting<Integer> hiddenAlpha = this.register(new Setting<Object>("Hidden Alpha", Integer.valueOf(90), Integer.valueOf(0), Integer.valueOf(255), v -> this.hiddenSync.getValue() == false));
+	public Setting<Color> hiddenC = this.register(new Setting<Color>("HiddenColor", new Color(40, 192, 255, 255), v -> this.hiddenSync.getValue() == false));
 
     public CrystalModify() {
         super("CrystalModify", "Modifies crystal rendering in different ways", Module.Category.RENDER, true, false, false);
